7d9bf795358e9bf415c91bb3abc9f11859f201eb
Matheus Brito <matheusb790@gmail.com>
2022-04-26 12:05:04 -0300

    Udpate

--- src/main/java/visao/Programa.java
+++ src/main/java/visao/Programa.java
@@ -14,10 +14,17 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("aulaJPA_Maven");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario u = em.find(Usuario.class, 1);
-		
-		
-		System.out.println(u.getNome());
+		Usuario u1 = new Usuario("Steve Rogers", "cap@gmail.com");
+		Usuario u2 = new Usuario("Tony Stark", "tony@gmail.com");
+		Usuario u3 = new Usuario("Bruce Banner", "hulk@gmail.com");
+		Usuario u4 = new Usuario("Thor", "strg_avenger@gmail.com");
+
+		em.getTransaction().begin();
+		em.persist(u1);
+		em.persist(u2);
+		em.persist(u3);
+		em.persist(u4);
+		em.getTransaction().commit();
 		
 		em.close();
 		emf.close();
